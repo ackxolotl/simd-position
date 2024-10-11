776a7b140441b1972cc13302d8cd623801dc9236
Simon Ellmann <simon.ellmann@tum.de>
2024-10-11 14:42:59 +0200

    Implement subsplit

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,22 +1,18 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use rand::{rngs::StdRng, Rng, SeedableRng};
 use std::hint::black_box;
 
-fn generate_random_bytes(buffer: &mut [u8]) {
-    let mut rng = StdRng::seed_from_u64(42);
-    rng.fill(buffer);
-}
-
 fn criterion_benchmark(c: &mut Criterion) {
-    // 64 KiB of data
-    let mut input = [1u8; 1 << 16];
-
-    generate_random_bytes(&mut input[1 << 7..]);
+    let data = "1|1|4|XvTC9cQ1BnpH3ue|OE|BARBARBAR|ik1yiPkonwJ|pAxmX3VomS5QzAP1K|SPtt5EcqNaafy|hD|525811111|9613662508411977|39|GC|50000.00|.4211|-10.00|1.00|0|0|OsfGdm8UJFMPvwSA3lASWL8Aq941eu2K9bB406Rzp3Z2L3W29631NhfBugH98eeF5JxGNJimKMtwhM0o2F8lKgEw3dPTOSj1cN1w6gTUUk585rG2QIEo0PGO5QZifJWePMXK7GvXrnFrrjeNgbO89eaQiYI2Z7HiSa7RI6bEOGpBxmZ2OHfPsZrzk023YIYksYQfkwAcWOS2vneHL3aoCRliI0HD7gVSt5QnMKOfKSW3GZdUFnK4IU4YFOsAz9G3GuzFWJlshTTGtHXcm8AfrDJKoHtU6jaj5tX8bcUob2O15SX1ayRbVMpp0GwZSZ48KvbAwL37xt9CLZL4mWNc0vMSg3WI9xpG7PVQ3XOb3nTyV7tZ\n".repeat(10000);
+    let input = data.as_bytes();
 
     // benchmarks
     let mut group = c.benchmark_group("simd-position");
     group.bench_function("next-newline-std", |b| b.iter(|| simd_position::next_newline_std(black_box(&input))));
     group.bench_function("next-newline-simd", |b| b.iter(|| simd_position::next_newline_simd(black_box(&input))));
+    group.bench_function("split-std", |b| b.iter(|| simd_position::split_std(black_box(&input))));
+    group.bench_function("split-simd", |b| b.iter(|| simd_position::split_simd(black_box(&input))));
+    group.bench_function("subsplit-std", |b| b.iter(|| simd_position::subsplit_std(black_box(&input))));
+    group.bench_function("subsplit-simd", |b| b.iter(|| simd_position::subsplit_simd(black_box(&input))));
     group.finish();
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -26,6 +26,82 @@ pub fn next_newline_simd(input: &[u8]) -> Option<usize> {
     None
 }
 
+pub fn split_std(input: &[u8]) -> Vec<&[u8]> {
+    input.split(|&i| i == b'\n').collect()
+}
+
+pub fn split_simd(input: &[u8]) -> Vec<&[u8]> {
+    let mut result = vec![];
+
+    let mask = u8x32::splat(b'\n');
+    let mut last = 0;
+
+    let mut pos = 0;
+    while pos < input.len() {
+        let data = Simd::<u8, 32>::load_or_default(&input[pos..]);
+        let mut positions = mask.simd_eq(data).to_bitmask();
+
+        while positions != 0 {
+            let bit = positions.trailing_zeros();
+            let offset = pos + bit as usize;
+
+            result.push(&input[last..offset]);
+
+            last = offset + 1;
+            positions ^= 1 << bit as u64;
+        }
+
+        pos += data.len();
+    }
+
+    result.push(&input[last..]);
+
+    result
+}
+
+pub fn subsplit_std(input: &[u8]) -> Vec<String> {
+    input.split(|&i| i == b'\n').map(|s| s.split(|&i| i == b'|')).flatten().map(|slice| std::str::from_utf8(slice).unwrap().to_string()).collect()
+}
+
+pub fn subsplit_simd(input: &[u8]) -> Vec<String> {
+    let mut result = vec![];
+    subsplit_simd_helper(input, b'\n', |i: &[u8]| {
+        subsplit_simd_helper(i, b'|', |o: &[u8]| {
+            let s = std::str::from_utf8(o).unwrap();
+            result.push(s.to_string());
+        });
+    });
+    result
+}
+
+pub fn subsplit_simd_helper<F>(input: &[u8], delimiter: u8, mut f: F)
+where
+    F: FnMut(&[u8]),
+{
+    let mask = u8x32::splat(delimiter);
+    let mut last = 0;
+
+    let mut pos = 0;
+    while pos < input.len() {
+        let data = Simd::<u8, 32>::load_or_default(&input[pos..]);
+        let mut positions = mask.simd_eq(data).to_bitmask();
+
+        while positions != 0 {
+            let bit = positions.trailing_zeros();
+            let offset = pos + bit as usize;
+
+            f(&input[last..offset]);
+
+            last = offset + 1;
+            positions ^= 1 << bit as u64;
+        }
+
+        pos += data.len();
+    }
+
+    f(&input[last..]);
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -49,6 +125,15 @@ mod tests {
         assert_eq!(next, Some(43));
     }
 
+    fn test_split<F>(func: F)
+    where
+        F: Fn(&[u8]) -> Vec<&[u8]>,
+    {
+        let input = b"hello\nrustaceans\nthisisaverylonginputstring\n";
+        let next = func(input);
+        assert_eq!(next, input.split(|&i| i == b'\n').collect::<Vec<_>>());
+    }
+
     #[test]
     fn test_next_newline_std() {
         test_next_newline(next_newline_std);
@@ -57,5 +142,22 @@ mod tests {
     #[test]
     fn test_next_newline_simd() {
         test_next_newline(next_newline_simd);
+    }
+
+    #[test]
+    fn test_split_std() {
+        test_split(split_std);
+    }
+
+    #[test]
+    fn test_split_simd() {
+        test_split(split_simd);
+    }
+
+    #[test]
+    fn test_subsplit_simd() {
+        let input = b"hel|o\nru|tace|ns\nthi|isaveryl|nginput|tring\n";
+        let next = subsplit_simd(input);
+        assert_eq!(next, subsplit_std(input));
     }
 }
